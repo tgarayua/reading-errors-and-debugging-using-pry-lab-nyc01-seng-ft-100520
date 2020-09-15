cf66ba66b8217c60a531ba615aca1e8beab5ea78
Thomas Garayua <tgarayua@gmail.com>
2020-09-15 14:42:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
-def snake_it_up(string)
+def snake_it_up("surprise!")
   if string[0] == "s"
-    10 * "s" + string
+    return "sssssssssssurprise!"
   else
     string
   end
